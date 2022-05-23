77ff54e650a1facef2c1fb9152084f84fd7446aa
Francois <francois.auclair.911@gmail.com>
2022-05-23 12:56:47 -0400

    Removing debug stuff

--- pages/_organization_id/_page_slug/index.vue
+++ pages/_organization_id/_page_slug/index.vue
@@ -2,15 +2,6 @@
   <v-row class="my-10 px-1 px-md-10">
     <v-col cols="12" md="10" offset-md="1">
       <v-row :justify="$vuetify.breakpoint.mobile ? 'center' : form_alignment">
-        <v-col cols="12">
-          <v-card color="primary">
-            <v-card-title class="white--text">
-              Hello this is {{ $config.PAYMENT_BASE_URL }} Hello this is
-              {{ $config.PAYMENT_BASE_URL }} Hello this is
-              {{ $config.PAYMENT_BASE_URL }}
-            </v-card-title>
-          </v-card>
-        </v-col>
         <v-col cols="12" sm="9" md="6" lg="5" xl="3" class="">
           <v-card elevation="10" :loading="requestState === 'pending'">
             <v-overlay absolute :value="requestState === 'pending'" z-index="4">
